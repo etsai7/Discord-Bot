a3178519f62b946077a6eb781fed7c65978d85ee
Eric Tsai <e.tsai70@gmail.com>
2024-01-03 22:22:08 -0500

    Adding Sub Commands for the test extension and modifying further embeds

--- src/exts/test_ext.py
+++ src/exts/test_ext.py
@@ -3,20 +3,33 @@ import src.credentials as credentials
 
 
 class TestExtension(Extension):
-    @slash_command(name='testext',
-                   description='Command to auto suggest a bunch of hypixel item choices',
-                   scopes=[credentials.discord_guild_id])
+    @slash_command(name='testing',
+                   scopes=[credentials.discord_guild_id],
+                   sub_cmd_name='testext',
+                   sub_cmd_description='Command to auto suggest a bunch of hypixel item choices')
     async def testExtension(self, ctx: SlashContext):
         await ctx.send("Hello Test Extension")
 
-    @slash_command(name='test_embed', description='Command to create an embed',
-                                scopes=[credentials.discord_guild_id])
-    async def testEmbed(ctx: SlashContext):
+    @testExtension.subcommand(sub_cmd_name='testembed',
+                              sub_cmd_description='Command to create an embed')
+    async def testEmbed(self, ctx: SlashContext):
         embed = Embed(
-            title="your title",
+            title="# **your title**",
             color=Color.from_rgb(201, 234, 252),
             description="your description",
             footer=EmbedFooter(text='This is a footer'),
-            timestamp=Timestamp.now())
+            timestamp=Timestamp.now(),
+            url='https://bongo.cat/')
         embed.set_thumbnail('https://i.pinimg.com/736x/52/fd/0c/52fd0ca64986b84c254726406cee5b6a.jpg')
+        embed.add_field('key1', 'value1', inline=False)
+        embed.add_field('key2', 'value2', inline=True)
+        embed.add_field('key3', 'value3', inline=True)
+        embed.add_field('Search', '[Google](https://www.google.com/)', inline=True)
         await ctx.send(embeds=embed)
+
+    @testExtension.subcommand(sub_cmd_name='testhyperlink',
+                              sub_cmd_description='Command to create a body of text with Hyperlink')
+    async def testHyperLink(selfself, ctx: SlashContext):
+        await ctx.send('Go to [Google](https://www.google.com/)')
+
+# Bongo Cat
