1f10548b89d2720f30f89d95d47ed8fd6db56463
Eric Tsai <e.tsai70@gmail.com>
2024-03-17 22:31:32 -0400

    Updating Ban Capabilities

--- src/exts/moderation/cmds/ban.py
+++ src/exts/moderation/cmds/ban.py
@@ -1,6 +1,7 @@
 from interactions import *
 from src.exts.moderation.moderation_ext import ModerationExtension
-from datetime import datetime
+from src.exts.tasks.moderation_tasks import ModerationTasks
+from datetime import datetime, timedelta
 
 
 class Ban(Extension):
@@ -10,10 +11,35 @@ class Ban(Extension):
                   description='The user to be banned',
                   opt_type=OptionType.USER,
                   required=True)
-    async def ban_user(self, ctx: SlashContext, user: Member):
-        await user.edit(communication_disabled_until=Timestamp(datetime.now()))
+    @slash_option(name='time',
+                  description='Duration of ban',
+                  opt_type=OptionType.INTEGER,
+                  required=True)
+    @slash_option(name='units',
+                  description='Time Unit',
+                  choices=[SlashCommandChoice(name='seconds', value=1),
+                           SlashCommandChoice(name='minutes', value=60),
+                           SlashCommandChoice(name='hours', value=3600),
+                           SlashCommandChoice(name='days', value=86400)],
+                  opt_type=OptionType.INTEGER,
+                  required=True)
+    async def ban_user(self, ctx: SlashContext, user: User, time: int, units: int):
+        # await user.edit(communication_disabled_until=Timestamp(datetime.now()))
         await ctx.guild.ban(user=user, reason='testing')
         await ctx.send('Banned!!')
+
+        current_datetime = datetime.now()
+        try:
+            new_datetime = current_datetime + timedelta(seconds=time*units)
+        except:
+            new_datetime = current_datetime + timedelta(seconds=86400)
+            print("Duration too long, defaulting to 1 year")
+
+        task = Task(ModerationTasks(self.bot, ctx, user).unban, DateTrigger(new_datetime))
+        task.start()
+        unix_time = int(datetime.timestamp(new_datetime))
+        await ctx.send(f"<t:{unix_time}:R>")
+
 
 
 def setup(bot):
--- src/exts/tasks/moderation_tasks.py
+++ src/exts/tasks/moderation_tasks.py
@@ -1,8 +1,22 @@
 from interactions import *
 
+
 class ModerationTasks(Extension):
-    def __init__(self, bot, ctx: SlashContext):
+    def __init__(self, bot, ctx: SlashContext, user: User):
         self.ctx = ctx
+        self.user = user
 
     async def unban(self):
-        await self.ctx.guild.unban(user=self.ctx.user, reason='Did the time')
+
+        await self.ctx.guild.unban(user=self.user, reason='Did the time')
+        print('Unbanning Members')
+
+        # --- This is not possible to re-invite/dm a person that is not in the Bot's server
+        # await self.bot.fetch_user(self.user.id).send('https://discord.gg/CS2v46enp8')
+        # usr = await self.bot.fetch_user(user_id=self.user.id)
+        # print(f'Got the user: {usr}')
+        # print(f'Original user: {self.user}')
+        # await usr.send(content='https://discord.gg/CS2v46enp8')
+        # await self.ctx.user.send(content='https://discord.gg/CS2v46enp8')
+        # await usr.send('https://discord.gg/CS2v46enp8')
+        # await self.ctx.channel.create_invite(max_uses=1, target_user=self.user, reason='Welcome back from exile')
