85b3a2db63d35c978b4cbbc73260bf68f7ffc2aa
Eric Tsai <e.tsai70@gmail.com>
2024-04-09 23:38:11 -0400

    Adding message storage for snipe

--- src/exts/moderation/cmds/snipe.py
+++ src/exts/moderation/cmds/snipe.py
@@ -1,22 +1,36 @@
 from interactions import *
-from interactions.api.events import MessageDelete
+from interactions.api.events import MessageDelete, MessageUpdate
 
 from src.exts.moderation.moderation_ext import ModerationExtension
 
 
 class Snipe(Extension):
     def __init__(self, bot):
-        self.deleted_message: MessageDelete = None
+        self.last_message: Message = None
+        self.stored_messages: list[Message] = []
+        self.max_stored_messages: int = 3
 
     @ModerationExtension.mod_extension.subcommand(sub_cmd_name='snipe',
                                                   sub_cmd_description='Retrieve the last deleted message')
     async def snipe(self, ctx: SlashContext):
-        await ctx.send(f'Last Message: {self.deleted_message.message.content}')
+        msg = None if len(self.stored_messages) <= 0 else self.stored_messages[0].content
+        await ctx.send(f'Last Message: {msg}')
+
+    @listen(MessageUpdate)
+    async def on_message_update(self, edited_message: MessageUpdate):
+        self.last_message = edited_message.before
+        self.stored_messages.insert(0, self.last_message)
+        if len(self.stored_messages) > self.max_stored_messages:
+            self.stored_messages = self.stored_messages[:self.max_stored_messages]
+        print(f'Updated message: {self.last_message.content}')
 
     @listen(MessageDelete)
     async def on_message_delete(self, deleted_message: MessageDelete):
-        self.deleted_message = deleted_message
-        print(f'Deleted message: {deleted_message.message.content}')
+        self.last_message = deleted_message.message
+        self.stored_messages.insert(0, self.last_message)
+        if len(self.stored_messages) > self.max_stored_messages:
+            self.stored_messages = self.stored_messages[:self.max_stored_messages]
+        print(f'Deleted message: {self.last_message.content}')
 
 
 def setup(bot):
